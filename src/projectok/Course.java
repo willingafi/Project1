fa470ff44e28e73b4c24d738e623ba21a24b8de1
Willy NGAFI <willyngafi2012@gmail.com>
2018-06-13 13:05:24 +0100

    Branches

---
+++ src/projectok/Course.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectok;
+
+/**
+ *
+ * @author willy-ngafi
+ */
+public class Course {
+    
+    String y;
+    int x;
+
+    public Course(String y, int x) {
+        this.y = y;
+        this.x = x;
+    }
+
+    public String getY() {
+        return y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setY(String y) {
+        this.y = y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+    
+    
+}
